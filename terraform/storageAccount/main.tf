b19ed6aba52e2c7b9304898e7ad4516bbf5fbc94
spectrum75 <107911114+spectrum75@users.noreply.github.com>
2023-03-25 17:09:07 +1300

    Feature/tf storage account (#11)
    * add sample storage account tf
    
    * add container to tf file

---
+++ terraform/storageAccount/main.tf
@@ -0,0 +1,75 @@
+# declare variables
+variable "resource_group_name" {
+  type        = string
+  description = "Name of an existing resource group"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Name of the storage account"
+}
+
+variable "storage_account_tier" {
+  type        = string
+  description = "Performance tier for the storage account"
+  default     = "Standard"
+}
+
+variable "storage_account_replication_type" {
+  type        = string
+  description = "Replication type for the storage account"
+  default     = "LRS"
+  validation {
+    condition     = contains(["LRS", "GRS", "RAGRS", "ZRS", "GZRS", "RAGZRS"], var.storage_account_replication_type)
+    error_message = "Not an allowed default value"
+  }
+}
+
+variable "ipAllowList" {
+  type        = list(string)
+  description = "Optional. Allow list of public IPs allowed to access storage account."
+  default     = []
+}
+
+variable "container_names" {
+  type        = list(string)
+  description = "List of Azure Blob container names to create"
+  default     = []
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                      = var.storage_account_name
+  resource_group_name       = data.azurerm_resource_group.rg.name
+  location                  = data.azurerm_resource_group.rg.location
+  account_tier              = var.storage_account_tier
+  account_replication_type  = var.storage_account_replication_type
+  enable_https_traffic_only = true
+  network_rules {
+    default_action = "Deny"
+    bypass         = ["AzureServices"]
+    ip_rules       = var.ipAllowList
+  }
+}
+
+resource "azurerm_storage_container" "containers" {
+  count                 = length(var.container_names)
+  name                  = var.container_names[count.index]
+  storage_account_name  = azurerm_storage_account.sa.name
+}
+
+output "storage_account_id" {
+  value = azurerm_storage_account.sa.id
+  description = "The resource ID of the deployed storage account."
+}
+
+output "container_names" {
+  value = azurerm_storage_container.containers.*.name
+}
