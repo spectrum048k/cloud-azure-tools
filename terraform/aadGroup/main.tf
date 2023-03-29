fa8b61c18d5b61d2693f2f48d2f91d3a9100f4e5
spectrum75 <107911114+spectrum75@users.noreply.github.com>
2023-03-29 22:28:03 +1300

    Feature/aad group (#14)
    * add aad group sample
    
    * add map variable and use for_each
    
    * remove unused variable

---
+++ terraform/aadGroup/main.tf
@@ -0,0 +1,37 @@
+# https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/group
+
+# Configure Terraform
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+# variables
+variable "groups" {
+  description = "Name/value map of Azure AD group names"
+  type = map
+}
+
+# Configure the Azure Active Directory Provider
+provider "azuread" {
+}
+
+data "azuread_client_config" "current" {}
+
+resource "azuread_group" "group" {
+  for_each = var.groups
+
+  display_name            = each.value
+  owners                  = [data.azuread_client_config.current.object_id]
+  security_enabled        = true
+  description             = "group created by terraform aad provider"
+  prevent_duplicate_names = true
+}
+
+output "group_id" {
+  value = values(azuread_group.group).*.id
+}
