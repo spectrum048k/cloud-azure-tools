61adc8811b2590c3db481746cde5f870ca853d30
spectrum048k <>
2023-06-11 14:32:31 +1200

    add new custom role

--- terraform/customRole/main.tf
+++ terraform/customRole/main.tf
@@ -1,25 +1,39 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0"
+    }
+  }
+}
 
 provider "azurerm" {
   features {}
 }
 
-data "azurerm_subscription" "main" {
+data "azurerm_subscription" "primary" {
 }
 
-resource "azurerm_role_definition" "custom_role" {
-  name               = "tf-custom-role-definition-01"
-  scope              = data.azurerm_subscription.main.id
+resource "azurerm_role_definition" "cloud_workload_operator" {
+  name  = "cr-cloud-workload-operator"
+  scope = data.azurerm_subscription.primary.id
 
   permissions {
-    actions     = ["Microsoft.Resources/subscriptions/resourceGroups/read"]
+    actions = [
+      "Microsoft.Compute/*/read",
+      "Microsoft.Compute/virtualMachines/start/action",
+      "Microsoft.Compute/virtualMachines/restart/action",
+      "Microsoft.Compute/virtualMachines/deallocate/action",
+      "Microsoft.Compute/virtualMachines/retrieveBootDiagnosticsData/action"
+    ]
     not_actions = []
   }
 
   assignable_scopes = [
-    data.azurerm_subscription.main.id,
+    data.azurerm_subscription.primary.id
   ]
 }
 
-output "role_id" {
-  value = azurerm_role_definition.custom_role.id
+output "cloud_workload_operator_id" {
+  value = azurerm_role_definition.cloud_workload_operator.id
 }
